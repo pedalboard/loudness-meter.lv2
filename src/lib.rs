77f1ea2caf810793ec4951bd0f2d0dcab0bcb083
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2023-04-01 22:03:35 +0200

    Use sample rate

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use lv2::prelude::*;
 use wmidi::*;
 
+const SAMPLE_RATE: u32 = 48000;
+
 #[derive(PortCollection)]
 struct Ports {
     gain: InputPort<InPlaceControl>,
@@ -24,6 +26,7 @@ pub struct URIDs {
 #[uri("https://github.com/pedalboard/db-meter.lv2")]
 struct DbMeter {
     urids: URIDs,
+    sample_count: u32,
 }
 
 impl Plugin for DbMeter {
@@ -35,10 +38,11 @@ impl Plugin for DbMeter {
     fn new(_plugin_info: &PluginInfo, features: &mut Features) -> Option<Self> {
         Some(Self {
             urids: features.map.populate_collection()?,
+            sample_count: 0,
         })
     }
 
-    fn run(&mut self, ports: &mut Ports, _features: &mut (), _: u32) {
+    fn run(&mut self, ports: &mut Ports, _features: &mut (), count: u32) {
         let gain = ports.gain.get();
         let coef = if gain > -90.0 {
             10.0_f32.powf(gain * 0.05)
@@ -52,20 +56,27 @@ impl Plugin for DbMeter {
         for (input_sample, output_sample) in input.zip(output) {
             output_sample.set(input_sample.get() * coef);
         }
-        let mut level_sequence = ports
-            .level
-            .write(self.urids.atom.sequence)
-            .unwrap()
-            .with_unit(self.urids.unit.frame)
-            .unwrap();
 
-        let message_to_send = MidiMessage::NoteOn(Channel::Ch1, Note::C1, Velocity::MAX);
+        self.sample_count += count;
 
-        level_sequence
-            .new_event(0, self.urids.midi.wmidi)
-            .unwrap()
-            .set(message_to_send)
-            .unwrap();
+        if self.sample_count > SAMPLE_RATE {
+            self.sample_count = self.sample_count.rem_euclid(SAMPLE_RATE);
+
+            let mut level_sequence = ports
+                .level
+                .write(self.urids.atom.sequence)
+                .unwrap()
+                .with_unit(self.urids.unit.frame)
+                .unwrap();
+
+            let message_to_send = MidiMessage::NoteOn(Channel::Ch1, Note::C1, Velocity::MAX);
+
+            level_sequence
+                .new_event(0, self.urids.midi.wmidi)
+                .unwrap()
+                .set(message_to_send)
+                .unwrap();
+        }
     }
 }
 
