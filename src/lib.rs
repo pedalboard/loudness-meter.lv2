f4345a9ddc5967b966938d66605f8c7c0184a36e
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2023-04-02 11:36:37 +0200

    Change gain to level output

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,10 @@ const SAMPLE_RATE: u32 = 48000;
 
 #[derive(PortCollection)]
 struct Ports {
-    gain: InputPort<InPlaceControl>,
     input: InputPort<InPlaceAudio>,
     output: OutputPort<InPlaceAudio>,
-    level: OutputPort<AtomPort>,
+    level: InputPort<InPlaceControl>,
+    level_midi: OutputPort<AtomPort>,
 }
 
 #[derive(FeatureCollection)]
@@ -27,6 +27,7 @@ pub struct URIDs {
 struct DbMeter {
     urids: URIDs,
     sample_count: u32,
+    count: f32,
 }
 
 impl Plugin for DbMeter {
@@ -39,31 +40,28 @@ impl Plugin for DbMeter {
         Some(Self {
             urids: features.map.populate_collection()?,
             sample_count: 0,
+            count: 0.0,
         })
     }
 
     fn run(&mut self, ports: &mut Ports, _features: &mut (), count: u32) {
-        let gain = ports.gain.get();
-        let coef = if gain > -90.0 {
-            10.0_f32.powf(gain * 0.05)
-        } else {
-            0.0
-        };
-
         let input = ports.input.iter();
         let output = ports.output.iter();
 
         for (input_sample, output_sample) in input.zip(output) {
-            output_sample.set(input_sample.get() * coef);
+            output_sample.set(input_sample.get());
         }
 
         self.sample_count += count;
 
         if self.sample_count > SAMPLE_RATE {
+            self.count = self.count + 1.0;
+            ports.level.set(self.count);
+
             self.sample_count = self.sample_count.rem_euclid(SAMPLE_RATE);
 
             let mut level_sequence = ports
-                .level
+                .level_midi
                 .write(self.urids.atom.sequence)
                 .unwrap()
                 .with_unit(self.urids.unit.frame)
