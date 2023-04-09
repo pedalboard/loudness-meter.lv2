e6c9db2271f8fcb68051b21324134e20640f16e6
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2023-04-09 20:51:24 +0200

    Define inputs and outputs

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,13 @@ const SAMPLE_RATE: u32 = 48000;
 
 #[derive(PortCollection)]
 struct Ports {
-    input: InputPort<InPlaceAudio>,
-    output: OutputPort<InPlaceAudio>,
-    level_midi: OutputPort<AtomPort>,
-    max: OutputPort<InPlaceControl>,
-    count: OutputPort<InPlaceControl>,
+    in_r: InputPort<InPlaceAudio>,
+    in_l: InputPort<InPlaceAudio>,
+    out_r: OutputPort<InPlaceAudio>,
+    out_l: OutputPort<InPlaceAudio>,
+    loudness_midi: OutputPort<AtomPort>,
+    short_term: OutputPort<InPlaceControl>,
+    integrated: OutputPort<InPlaceControl>,
 }
 
 #[derive(FeatureCollection)]
@@ -29,9 +31,6 @@ pub struct URIDs {
 struct DbMeter {
     urids: URIDs,
     sample_count: u32,
-    event_count: u32,
-    on: bool,
-    max: f32,
 }
 
 impl Plugin for DbMeter {
@@ -44,46 +43,32 @@ impl Plugin for DbMeter {
         Some(Self {
             urids: features.map.populate_collection()?,
             sample_count: 0,
-            on: false,
-            event_count: 0,
-            max: 0.0,
         })
     }
 
     fn run(&mut self, ports: &mut Ports, _features: &mut (), count: u32) {
-        let input = ports.input.iter();
-        let output = ports.output.iter();
-
+        /*
         for (input_sample, output_sample) in input.zip(output) {
             let value = input_sample.get();
-            if value.abs() > self.max {
-                self.max = value
-            }
             output_sample.set(value);
         }
+        */
 
         self.sample_count += count;
 
         if self.sample_count > SAMPLE_RATE {
-            self.on = !self.on;
-            self.event_count += 1;
-            ports.count.set(self.event_count as f32);
-            ports.max.set(self.max);
-
             self.sample_count = self.sample_count.rem_euclid(SAMPLE_RATE);
 
             let mut level_sequence = ports
-                .level_midi
+                .loudness_midi
                 .init(
                     self.urids.atom.sequence,
                     TimeStampURID::Frames(self.urids.unit.frame),
                 )
                 .unwrap();
 
-            let message_to_send = match self.on {
-                true => MidiMessage::NoteOn(Channel::Ch1, Note::C1, U7::try_from(1u8).unwrap()),
-                false => MidiMessage::NoteOff(Channel::Ch1, Note::C1, U7::try_from(1u8).unwrap()),
-            };
+            let message_to_send =
+                MidiMessage::NoteOff(Channel::Ch1, Note::C1, U7::try_from(1u8).unwrap());
 
             level_sequence
                 .init(
