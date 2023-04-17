37800556992e681f60c5012f989e930f31001ef7
laenzi <laenzi>
2023-04-17 21:46:54 +0100

    Add momentary loudness, fix requency of updates

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ struct Ports {
     out_r: OutputPort<InPlaceAudio>,
     out_l: OutputPort<InPlaceAudio>,
     loudness_midi: OutputPort<AtomPort>,
+    momentary: OutputPort<InPlaceControl>,
     short_term: OutputPort<InPlaceControl>,
     integrated: OutputPort<InPlaceControl>,
 }
@@ -30,6 +31,7 @@ pub struct URIDs {
 struct LoudnessMeter {
     urids: URIDs,
     sample_count: u32,
+    houndred_ms_count: u32,
     ebu: ebur128::EbuR128,
 }
 
@@ -44,7 +46,8 @@ impl Plugin for LoudnessMeter {
         Some(Self {
             urids: features.map.populate_collection()?,
             sample_count: 0,
-            ebu: EbuR128::new(2, sample_rate, Mode::S | Mode::I).unwrap(),
+            houndred_ms_count: 0,
+            ebu: EbuR128::new(2, sample_rate, Mode::S | Mode::I | Mode::M).unwrap(),
         })
     }
 
@@ -63,15 +66,17 @@ impl Plugin for LoudnessMeter {
         }
 
         self.sample_count += count;
+        let momentary = self.ebu.loudness_momentary().unwrap();
+        ports.momentary.set(momentary as f32);
 
-        // update the with 1Hz frequency
-        if self.sample_count > self.ebu.rate() {
-            self.sample_count = self.sample_count.rem_euclid(self.ebu.rate());
+        // update the short term loudness with 10Hz frequency
+        let rate = self.ebu.rate() / 10;
+        if self.sample_count > rate {
+            self.sample_count = self.sample_count.rem_euclid(rate);
+            self.houndred_ms_count += 1;
+
             let short_term = self.ebu.loudness_shortterm().unwrap();
-            let integrated = self.ebu.loudness_global().unwrap();
             ports.short_term.set(short_term as f32);
-            ports.integrated.set(integrated as f32);
-
             let mut level_sequence = ports
                 .loudness_midi
                 .init(
@@ -81,19 +86,29 @@ impl Plugin for LoudnessMeter {
                 .unwrap();
 
             let st = short_term.abs().min(127.0).round() as u8;
-            let int = integrated.abs().min(127.0).round() as u8;
-
             let st_message =
                 MidiMessage::NoteOff(Channel::Ch4, Note::C1, U7::try_from(st).unwrap());
+            level_sequence
+                .init(
+                    TimeStamp::Frames(self.sample_count as i64),
+                    self.urids.midi.wmidi,
+                    st_message,
+                )
+                .unwrap();
 
-            let int_message =
-                MidiMessage::NoteOff(Channel::Ch4, Note::D1, U7::try_from(int).unwrap());
-            level_sequence
-                .init(TimeStamp::Frames(100), self.urids.midi.wmidi, st_message)
-                .unwrap();
-            level_sequence
-                .init(TimeStamp::Frames(100), self.urids.midi.wmidi, int_message)
-                .unwrap();
+            // update integrated loudness with 1Hz frequency
+            if self.houndred_ms_count > 10 {
+                self.houndred_ms_count = 0;
+                let integrated = self.ebu.loudness_global().unwrap();
+                ports.integrated.set(integrated as f32);
+                let int = integrated.abs().min(127.0).round() as u8;
+                let int_message =
+                    MidiMessage::NoteOff(Channel::Ch4, Note::D1, U7::try_from(int).unwrap());
+
+                level_sequence
+                    .init(TimeStamp::Frames(100), self.urids.midi.wmidi, int_message)
+                    .unwrap();
+            }
         }
     }
 }
